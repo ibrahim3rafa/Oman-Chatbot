6a0b0393fb2e30528d602e8470af3f4154e6d5bc
ibrahim3rafa <ibrahimarafa611@gmail.com>
2026-03-13 15:26:37 +0200

    refactoring

--- src/main/java/pages/ChatbotPage.java
+++ src/main/java/pages/ChatbotPage.java
@@ -1,43 +1,44 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+import java.util.List;
 
 public class ChatbotPage {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private WebElement shadowHost;
+    private SearchContext shadowRoot;
 
 
     private By inputField = By.id("chat-input");
     private By sendButton = By.id("send-btn");
     private By lastBotMessage = By.xpath("(//div[@class='message-bubble'])[last()]");
 
-    public ChatbotPage(WebDriver driver) {
+    public ChatbotPage(WebDriver driver, WebDriverWait wait, WebElement shadowHost, SearchContext shadowRoot) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        this.wait = wait;
+        this.shadowHost = shadowHost;
+        this.shadowRoot = shadowRoot;
+
     }
 
     public void sendQuestion(String question) {
 
-        WebElement input = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(inputField));
-        input.clear();
-        input.sendKeys(question);
-
-        driver.findElement(sendButton).click();
+        WebElement chatInput = wait.until(d -> shadowRoot.findElement(By.cssSelector("#chat-input")));
+        chatInput.clear();
+        chatInput.sendKeys(question);
+        WebElement sendBtn = shadowRoot.findElement(By.cssSelector("#send-btn"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", sendBtn);
     }
 
-    public String getBotAnswer() {
-
-        WebElement answer = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(lastBotMessage));
-
+    public String getBotAnswer() throws InterruptedException {
+        wait.until(d -> shadowRoot.findElement(By.cssSelector("#chat-input")).getAttribute("placeholder").equals("اكتب رسالتك هنا..."));
+        List<WebElement> answers = wait.until(d -> shadowRoot.findElements(By.cssSelector("#message-bubble")));
+        //get last message which is the answer
+        WebElement answer = answers.getLast();
         return answer.getText();
     }
 }
--- src/test/java/tests/ChatbotExcelTest.java
+++ src/test/java/tests/ChatbotExcelTest.java
@@ -1,14 +1,14 @@
 package tests;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import pages.ChatbotPage;
 import utils.ExcelUtil;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
 
 import java.time.Duration;
 
@@ -18,21 +18,41 @@ public class ChatbotExcelTest {
     private WebDriverWait wait;
     private ChatbotPage chatbot;
     private ExcelUtil excel;
-    private By chatButton = By.id("chatbot-button");
-    private By disclaimerAcceptButton = By.id("disclaimer-accept");
+    private WebElement shadowHost;
+    private SearchContext shadowRoot;
 
     @BeforeClass
     public void setup() throws Exception {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         driver.manage().window().maximize();
-        driver.get("https://journalism-roulette-ranch-written.trycloudflare.com/");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(chatButton));
-        driver.findElement(chatButton).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(disclaimerAcceptButton));
-        driver.findElement(disclaimerAcceptButton).click();
-        chatbot = new ChatbotPage(driver);
+        driver.get("http://87.121.168.132/");
+
+        // wait for shadow host
+        shadowHost = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("e-chat"))
+        );
+
+        // get shadow root
+        shadowRoot = shadowHost.getShadowRoot();
+
+        // wait for chat button
+        WebElement chatButton = wait.until(d ->
+                shadowRoot.findElement(By.cssSelector("button"))
+        );
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", chatButton);
+
+        // wait for disclaimer accept button
+        WebElement startChat = wait.until(d ->
+                shadowRoot.findElement(By.cssSelector("#disclaimer-accept"))
+        );
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", startChat);
+
+        chatbot = new ChatbotPage(driver, wait, shadowHost, shadowRoot);
         excel = new ExcelUtil("chatbot_data.xlsx");
+
     }
 
     @Test
